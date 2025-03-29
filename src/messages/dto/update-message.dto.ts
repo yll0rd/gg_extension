fe8f799edcd8f44ddbfa69d90250ef0023f0a64f
mijinummi <tech.md.auwal@gmail.com>
2025-03-29 10:45:46 +0000

    feat(messages): Implement read receipt functionality

--- src/messages/dto/update-message.dto.ts
+++ src/messages/dto/update-message.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateMessageDto } from './create-message.dto';
+import { IsBoolean, IsOptional } from 'class-validator';
 
-export class UpdateMessageDto extends PartialType(CreateMessageDto) {}
+export class UpdateMessageDto extends PartialType(CreateMessageDto) {
+  @IsOptional()
+  @IsBoolean()
+  isRead?: boolean;
+}
--- src/messages/entities/message.entity.ts
+++ src/messages/entities/message.entity.ts
@@ -1,45 +1,53 @@
 import {
-  Entity,
-  PrimaryGeneratedColumn,
-  Column,
-  Index,
-  CreateDateColumn,
-} from 'typeorm';
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    Index,
+    CreateDateColumn, OneToMany,
+  } from 'typeorm';
+  import { MessageReadReceipt } from './message-read-receipt.entity';
+  
+  export enum MessageType {
+    TEXT = 'text',
+    MEDIA = 'media',
+    TOKEN_TRANSFER = 'token_transfer',
+  }
+  
+  @Entity('messages')
+  export class Message {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+  
+    @Column('text')
+    content: string;
+  
+    @Column({
+      type: 'enum',
+      enum: MessageType,
+      default: MessageType.TEXT,
+    })
+    type: MessageType;
+  
+    @Column('uuid')
+    @Index('idx_message_sender')
+    senderId: string;
+  
+    @Column('uuid')
+    @Index('idx_message_conversation')
+    conversationId: string;
+  
+    @CreateDateColumn({ type: 'timestamp with time zone' })
+    @Index('idx_message_timestamp')
+    timestamp: Date;
+  
+    @Column('jsonb', { nullable: true })
+    metadata: Record<string, any>;
 
-export enum MessageType {
-  TEXT = 'text',
-  MEDIA = 'media',
-  TOKEN_TRANSFER = 'token_transfer',
-}
+    @Column({ type: 'uuid', array: true, default: [] }) // Add this line
+  readByUsers: string[]; // List of user IDs who have read the message
 
-@Entity('messages')
-export class Message {
-  @PrimaryGeneratedColumn('uuid')
-  id: string;
+  @OneToMany(() => MessageReadReceipt, (receipt) => receipt.message)
+readReceipts: MessageReadReceipt[];
 
-  @Column('text')
-  content: string;
-
-  @Column({
-    type: 'enum',
-    enum: MessageType,
-    default: MessageType.TEXT,
-  })
-  type: MessageType;
-
-  @Column('uuid')
-  @Index('idx_message_sender')
-  senderId: string;
-
-  @Column('uuid')
-  @Index('idx_message_conversation')
-  conversationId: string;
-
-  @CreateDateColumn({ type: 'timestamp with time zone' })
-  @Index('idx_message_timestamp')
-  timestamp: Date;
-
-  // Optional: Additional metadata for media or token transfer
-  @Column('jsonb', { nullable: true })
-  metadata: Record<string, any>;
-}
+  }
+  
